3afb8a4d2dcf34eeead6173a6f6ef5cc3d92a181
Jesse Rushlow <40327885+jrushlow@users.noreply.github.com>
2020-02-06 15:19:46 -0500

    add ci database

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsPasswordResetBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
